fad7cfaed1d4d7c72a208c5f0e325efef8edfb35
siva krishnan <sivasudev2022@gmail.com>
2024-03-08 14:28:21 +0530

    commit-2

---
+++ Models/Directions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testrobo.Models
+{
+    public class Directions
+    {
+        public enum enumDirections
+        {
+            South = 0,
+            East = 1,
+            North = 2,
+            West = 3,
+        }
+    }
+}
---
+++ Models/Units.cs
@@ -0,0 +1,17 @@
+﻿
+
+using static testrobo.Models.Directions;
+
+namespace testrobo.Models
+{
+    public class Units
+    {
+        public int xUnit { get; set; } = 5;
+        public int yUnit { get; set; } = 5;
+        public string directions { get; set; } = "North";
+
+        public int setSelectedRow { get; set; } = 0;
+        public int setSelectedColumn { get; set; } = 0;
+    }
+}
+
